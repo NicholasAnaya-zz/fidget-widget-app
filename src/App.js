d8210f563109f9e41d33c64eb9916bc08bf40749
NickAnaya618 <anaya.nicholas@gmail.com>
2017-05-01 15:18:21 -0400

    fidget spinner now in center of page, scroll is now locked for future functionality

--- src/App.js
+++ src/App.js
@@ -7,12 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Fidget-Widget</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-spinner">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
       </div>
     );
   }
